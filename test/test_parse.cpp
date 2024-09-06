adcb472bc1b7596a81dac975470a5b24ae55a1e7
Richard <rs@directx9.de>
2024-09-05 20:26:52 -0600

    Add Catch2 v3

--- test/test_parse.cpp
+++ test/test_parse.cpp
@@ -2,9 +2,9 @@
 #include <string_view>
 
 
-std::string_view bmsOutput = 
-"Power Volt   Curr   Tempr  Tlow   Thigh  Vlow   Vhigh  Base.St  Volt.St  Curr.St  Temp.St  Coulomb  Time                 B.V.St   B.T.St   Barcode              DevType             
-1     52557  -2698  31000  29000  30000  3283   3287   Dischg   Normal   Normal   Normal   57%      2024-09-06 09:42:07  Normal   Normal   ES1000899P021133     E-BOX-48100R-C     "
+std::string_view bmsOutput = R"(
+Power Volt   Curr   Tempr  Tlow   Thigh  Vlow   Vhigh  Base.St  Volt.St  Curr.St  Temp.St  Coulomb  Time                 B.V.St   B.T.St   Barcode              DevType             
+1     52557  -2698  31000  29000  30000  3283   3287   Dischg   Normal   Normal   Normal   57%      2024-09-06 09:42:07  Normal   Normal   ES1000899P021133     E-BOX-48100R-C     
 2     52568  -2767  31000  29000  30000  3281   3288   Dischg   Normal   Normal   Normal   57%      2024-09-06 09:42:05  Normal   Normal   ES1000899P021107     E-BOX-48100R-C      
 3     52566  -2763  30000  29000  29000  3283   3288   Dischg   Normal   Normal   Normal   57%      2024-09-06 09:42:04  Normal   Normal   ES1000899P021108     E-BOX-48100R-C      
 4     52532  -2998  29000  27000  28000  3281   3286   Dischg   Normal   Normal   Normal   57%      2024-09-06 09:42:04  Normal   Normal   ES1000899P021132     E-BOX-48100R-C      
@@ -20,4 +20,4 @@ std::string_view bmsOutput =
 14    -      -      -      -      -      -      -      Absent   -        -        -        -        -                    -        -        -                    -                   
 15    -      -      -      -      -      -      -      Absent   -        -        -        -        -                    -        -        -                    -                   
 16    -      -      -      -      -      -      -      Absent   -        -        -        -        -                    -        -        -                    -                   
-";
+)";
